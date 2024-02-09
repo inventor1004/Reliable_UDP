b3d6b9ec25c5bc4f38e9af2ec0a5cb9e45a0b610
jongeon <eonesty@naver.com>
2024-02-09 16:50:39 -0500

    add header for PacketHandler.cpp

--- PacketHandler.cpp
+++ PacketHandler.cpp
@@ -1,2 +1,8 @@
-#include <stdio.h>
-
+/* ---------------------------------------------------------------------------------------- *
+* File Name  : PacketHandler.cpp
+* Author	 : Honggyu Park & Jongeon Lee
+* Date		 : 2024-01-26
+* Description: This file is related to the PacketHandler.cpp
+*			   This file includes the funcitons to make a packet from PacketHandler.cpp
+*				>> Implements 
+* ----------------------------------------------------------------------------------------- */ 
