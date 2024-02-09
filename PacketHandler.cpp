7140eb0db06f958388be416d2cad4c9212ceabeb
jongeon <eonesty@naver.com>
2024-02-09 16:57:45 -0500

    add checksum function

--- PacketHandler.cpp
+++ PacketHandler.cpp
@@ -6,3 +6,20 @@
 *			   This file includes the funcitons to make a packet from PacketHandler.cpp
 *				>> Implements 
 * ----------------------------------------------------------------------------------------- */ 
+#include <string>
+#include <stdint.h>
+
+#pragma warning(disable: 4996)
+
+using namespace std;
+
+unsigned char calculateChecksum(char* sendData, int dataSize)
+{
+    unsigned char sum = 0;
+
+    for (int i = 0; i < dataSize; ++i) {
+        sum += sendData[i];
+    }
+
+    return ~sum & 0xFF;
+}
